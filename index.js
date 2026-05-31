5d105538e1fea27367b94466713115df787762ce
shuvo2712 <shuvo2712@gmail.com>
2026-05-31 20:37:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Blood donation server is running');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
